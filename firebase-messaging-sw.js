c5b4d81a267d3faba040ca4cf7bc9e03d72b9434
valaa <kapiro@ukr.net>
2017-11-01 23:28:09 +0200

    Update firebase-messaging-sw.js

--- firebase-messaging-sw.js
+++ firebase-messaging-sw.js
@@ -3,7 +3,7 @@ importScripts('https://www.gstatic.com/firebasejs/3.6.8/firebase-app.js');
 importScripts('https://www.gstatic.com/firebasejs/3.6.8/firebase-messaging.js');
 
 firebase.initializeApp({
-    messagingSenderId: '887889079467'
+    messagingSenderId: 'AIzaSyBx4VEp3gWBCzNU69lpWq1UOPK_5MBnCXE'
 });
 
 const messaging = firebase.messaging();
